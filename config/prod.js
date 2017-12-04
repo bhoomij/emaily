ebbe88db592e1d180ad3ad66a36a8ae17bddb98f
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2017-12-05 03:30:05 +0530

    Fix heroku proxy issue

--- config/prod.js
+++ config/prod.js
@@ -4,9 +4,3 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
 }
-
-// mongodb://bhoomi:bhoomi_emaily_prod_1@ds117605.mlab.com:17605/emaily-prod-1
-// 770427520598-h6vhoe1sbfst11hk9hdm8fd1hgj2hi92.apps.googleusercontent.com
-// Client secret	
-// Jr9Te7lhLq7cGN76Lgr1ibpd
-// aslsmzcozcmvnfdkvmos
