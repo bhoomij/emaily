a8a37fb19c239279314a4025dbfb9be600921227
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2017-12-05 03:21:34 +0530

    Add google auth

---
+++ config/prod.js
@@ -0,0 +1,12 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+}
+
+// mongodb://bhoomi:bhoomi_emaily_prod_1@ds117605.mlab.com:17605/emaily-prod-1
+// 770427520598-h6vhoe1sbfst11hk9hdm8fd1hgj2hi92.apps.googleusercontent.com
+// Client secret	
+// Jr9Te7lhLq7cGN76Lgr1ibpd
+// aslsmzcozcmvnfdkvmos
--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+const keys = require('./config/keys');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
+
+require('./services/passport');
+const authRoutes = require('./routes/auth');
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-})
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Emaily server started on port ${PORT}`);
